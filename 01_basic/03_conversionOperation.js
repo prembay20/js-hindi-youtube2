ff5aac79a6b9057369d12d5d737013674d5e932d
prembay20 <prem.bay20@gmail.com>
2026-05-27 12:19:52 +0000

    coversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn=""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn="hello world"
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn=0
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//simple note
+// 1 => true; 
+// 0 => false;
+// "" => false
+// "hello world" => true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
