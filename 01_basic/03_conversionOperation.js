5c229f003fd204fac37b106b5a15d9ddaaf9e9a6
prembay20 <prem.bay20@gmail.com>
2026-05-28 06:16:07 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -2,6 +2,11 @@
 // let booleanIsLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
+// let isLoggedIn=0
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
 // let isLoggedIn=""
 // let booleanIsLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
@@ -10,9 +15,6 @@
 // let booleanIsLoggedIn=Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-// let isLoggedIn=0
-// let booleanIsLoggedIn=Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
 
 //simple note
 // 1 => true; 
@@ -20,7 +22,51 @@
 // "" => false
 // "hello world" => true
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+ 
+// operation
+
+// let value=3
+// let neg = -value
+// console.log(neg);
+
+
+// string addition
+
+let str1="Hello"
+let str2=" world"
+let str3=str1+str2
+console.log(str3);
+ 
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log( 2 ** 3 ) // power hoo  2 ko power 2 (2*2)=4  and 2 ko power 3 (2*2*2)=8
+console.log(2/2)
+console.log(2%2)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2+ "2")
+
+console.log(3+4*5/2)
+
+console.log((3+4)*5/2)
+
+let num1,num2,num3
+num1= num2 = num3 =2+2
+
+let gameCounter = 100
+++gameCounter //prefix
+gameCounter++ // postfix
+console.log(gameCounter);
+
+
+
+
+
+
