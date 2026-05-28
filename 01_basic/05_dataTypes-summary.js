31a395a71e4ed95980ae5b376de338a51fd05f6d
prembay20 <prem.bay20@gmail.com>
2026-05-28 08:44:27 +0000

    Typesof datatypes with example

---
+++ 01_basic/05_dataTypes-summary.js
@@ -0,0 +1,61 @@
+ // Types of dataTypes
+
+ // 1. primitive  data types 
+    // 7 types : string, Number, Boolean,  Null, undefined, Symbol, BigInt
+
+    let str="hello world"
+    console.log(str)
+    console.log(typeof str)
+
+    let num=34
+    console.log(num)
+    console.log(typeof num)
+
+    let isLoggedIn=true
+    console.log(isLoggedIn)
+    console.log(typeof isLoggedIn)
+     
+    let outsideTemp=null
+    console.log(outsideTemp)
+    console.log(typeof outsideTemp)
+
+
+    let userEmail ;
+    console.log(userEmail)
+    console.log(typeof userEmail)
+
+
+    let id=Symbol('123')
+    let anotherId=Symbol('123')
+    console.log(id == anotherId)
+
+    const BigNum=7486747743743463738n
+    console.log(BigNum)
+    console.log(typeof BigNum)
+
+    
+    
+
+
+ // 2. Non primitive data type (Reference)
+ 
+ // Array, Objects, function
+
+  const arr=["Nepal", "India", "chaina"]
+  console.log(arr)
+  console.log(typeof arr)
+
+  let myObj={
+    name:"prem",
+    age:19,
+    address:"Birgunj"
+  }
+   console.log(myObj)
+   console.log(typeof myObj)
+   
+
+  const myFunction = function(){
+    console.log("Hello world");
+  }
+  console.log(typeof myFunction)
+  console.log( myFunction)
