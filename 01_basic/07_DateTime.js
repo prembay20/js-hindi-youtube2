6da8f4f782bbb68c50975305b547718faed120d4
prembay20 <prem.bay20@gmail.com>
2026-05-29 08:35:37 +0000

    Date & time Methods

---
+++ 01_basic/07_DateTime.js
@@ -0,0 +1,12 @@
+let myDate= new Date()
+console.log(myDate)
+console.log(typeof myDate)
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.getDate())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+
+
+
