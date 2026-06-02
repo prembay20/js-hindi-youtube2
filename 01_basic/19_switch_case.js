e45f169cfb05e41348b8dfe8413072f4641f49d1
prembay20 <prem.bay20@gmail.com>
2026-06-02 09:38:58 +0000

    Switch Case in js

---
+++ 01_basic/19_switch_case.js
@@ -0,0 +1,52 @@
+// Switch Case in javascript
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// // example
+// const month=3
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("febuary")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+    
+//     default:
+//         console.log("defult case match")
+//         break;
+// }
+
+// example with string data type
+const month="march"
+switch (month) {
+    case "january":
+        console.log("january")
+        break;
+    case "febuary":
+        console.log("febuary")
+        break;
+    case "march":
+        console.log("march")
+        break;
+    case "april":
+        console.log("april")
+        break;
+    
+    default:
+        console.log("defult case match")
+        break;
+}
