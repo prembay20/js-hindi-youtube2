8c14bd29e9cb9a17a8ce6e3222f63e8965b8667b
prembay20 <prem.bay20@gmail.com>
2026-06-03 13:11:24 +0000

    for in loop js

---
+++ 01_basic/24_for_in_loop.js
@@ -0,0 +1,19 @@
+// for in loop
+
+const myObject={
+    js:'javascript',
+    cpp:"C++",
+    rb:'ruby',
+    swift:'swift by apple'
+}
+
+for (const key in myObject) {
+   console.log(`${key} shortcut is for ${myObject[key]}`)
+}
+
+
+const programing=["js","rb","py","java","cpp"]
+
+for (const key in programing){
+    console.log(programing[key])
+}
---
+++ 01_basic/25_for_each_loop.js
@@ -0,0 +1,12 @@
+//most important and most useful loop >>>>>>> forEach loop in js
+
+
+// let coding =["js","ruby","java","python","cpp"]
+// // coding.forEach( function (val){
+// //      console.log(val)
+// // } )
+
+let coding =["js","ruby","java","python","cpp"]
+coding.forEach( function (item){
+     console.log(item)
+} )
