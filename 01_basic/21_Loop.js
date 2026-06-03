60d4d8d799ea763cb0653ac79a1629d3edff8f6a
prembay20 <prem.bay20@gmail.com>
2026-06-03 09:19:25 +0000

    loop, nested loop, loop with if else, loop with array, break and continue in js

--- 01_basic/21_Loop.js
+++ 01_basic/21_Loop.js
@@ -18,7 +18,8 @@
 //         }
 //         //   console.log(i)
     // }
-
+    
+    
     // for (let i = 0; i <=10; i++) {
     //     console.log(`outer loop value:${i}`)
     //     for (let j = 0; j <= 10; j++) {
