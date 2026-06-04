d69c267d3b7ef1c8dc119dbc454cc17f695215c7
prembay20 <prem.bay20@gmail.com>
2026-06-04 05:44:14 +0000

    datatypes in js

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+
+"use strict"// treat all JS code as newer version 
+console.log(3+3) // but
+alert(3+3) // we are using node js not browser
+
+// DATATYPES 
+
+let name="prem "
+let age= 18
+let isLoggedIn=false
+
+// number => 2 to power 53
+// bigInt
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+// undefined
+// symbol => unique
+
+
+//Object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
