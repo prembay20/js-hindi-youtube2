c1b94a491829da61df1a2f91aaae78cc7acbe4b1
prembay20 <prem.bay20@gmail.com>
2026-06-16 13:06:23 +0000

    for each loop in js

---
+++ Revesion/14_forEach_loop.js
@@ -0,0 +1,39 @@
+// forEach Loop
+const  coding =["js","java","c","python","cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val)
+// })
+
+// forEach Arrow function
+// coding.forEach(  (val)=>{
+//     console.log(val)
+// })
+
+// function printMe(item){
+//     console.log(item)
+// }
+// coding.forEach(printMe)
+
+coding.forEach( (item,index,arr)=>{
+    console.log(item,index,arr);
+} )
+
+const myCoding =[
+    {
+        languageName:"javaScript",
+        languageFileName:"js"
+    },
+     {
+        languageName:"python",
+        languageFileName:"py"
+    },
+     {
+        languageName:"java",
+        languageFileName:"java"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName)
+})
