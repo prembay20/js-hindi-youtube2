0e0eb38234a453fe5c5a33550c71f62a4864938f
prembay20 <prem.bay20@gmail.com>
2026-06-16 13:05:29 +0000

    for loop

---
+++ Revesion/11_loop.js
@@ -0,0 +1,17 @@
+// For Loop 
+// for(i=1; i <= 10; i++){
+//     console.log(i)
+//     for(j=1; j <= 10; j++){
+//     console.log(i + '*' + j + '=' + i*j)
+//     }
+// }        
+
+
+// for loop with break and continue
+for(let i=1; i <=20; i++){
+    if(i == 5){
+        console.log(` 5 is Detected `);
+      continue
+    }
+   console.log(` value of i is ${i}`);
+}
