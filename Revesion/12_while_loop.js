a81e1e5b01528e016ebae9c115b6d927d8eeb7dd
prembay20 <prem.bay20@gmail.com>
2026-06-16 13:05:47 +0000

    while loop

---
+++ Revesion/12_while_loop.js
@@ -0,0 +1,22 @@
+let i=0;
+while (i <= 10) {
+    console.log(i)
+   // i++
+    i = i+2
+}
+
+let myArray=["flash","batman","superman"];
+let arr= 0;
+while(arr < myArray.length){
+    console.log(`value is ${myArray[arr]}`)
+    arr = arr +2 // increament
+}
+
+
+// do while loop
+let score =1
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score <= 10); // Condition
+
